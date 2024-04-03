f5185794c484d1bfdfafcc37e55497dd7f798a3e
Kopernikus <128999240+Kopernikus73@users.noreply.github.com>
2024-04-03 16:06:58 +0200

    Update main.py
    Version 0.6 update

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from random import randint
-
+import sys
+import time
 #bombs_placed = 0
 bombs = 0
 Layout = []
 shown_Layout = []
 title = f'\033[21m          \033[0m \n\033[21mMINESEATER\033[0m'
-rules = f'\033[3meg. (0,1) -> 01\033[0m'
+format = f'\033[3meg. (0,1) -> 01\033[0m'
+check = ""
+did = False
+
+
+print("loading...")
+time.sleep(2)
 
 print(title + '\n')
-print(rules)
-
+print(format)
 length = int(input('\nPlease select a grid size(recommended 3-7)\n<grid-size> '))
 #bombs = int(input('\nPlease set the number of bombs\n<bombs> '))
-
+    
 class field:
     def __init__(self,coord,bomb,bomb_nearby,checked,detected):
         self.coord = coord
@@ -78,18 +84,11 @@ for i in range(length):
     for k in range(length):
         if Layout[i][k].bomb:
             bombs += 1
-while bombs == 0:
+while bombs == 0 or bombs == length**2:
     bomb_define_again()
 
 print(f'\nbombs : {bombs}')
 
-def end(win):
-    print("\nDas Spiel ist vorbei!\n")
-    if win == False:
-        print("Du hast verloren!")
-    if win == True:
-        print("Du hast gewonnen!\n")
-    input()
 
 def game(show):
     output = ""
@@ -104,11 +103,14 @@ def game(show):
 
 
 def check_field():
+    global did
     check = input('<mineseater> ')
     go_on = 0
     bombs_detected = 0
     if check == "show":
         game(True)
+    if check == "exit":
+        return
     else:
         try:
             for i in range(length):
@@ -119,7 +121,7 @@ def check_field():
                             end(False)
                             return
                         if Layout[i][k].bomb == False:
-                            print(f"Anliegende Bomben: {Layout[i][k].bomb_nearby}\n")
+                            print(f"Adjacent bombs: {Layout[i][k].bomb_nearby}\n")
                             Layout[i][k].checked = True
                     if Layout[i][k].checked == True or Layout[i][k].bomb_nearby == "/":
                         go_on += 1
@@ -129,18 +131,59 @@ def check_field():
                         bombs_detected += 1
                     else:
                         pass
-            if go_on >= (length**2):
-                end(True)
-            elif bombs_detected == bombs:
-                end(True)
-            else:
-                check_field()
         except:
-            print(f"input error >>>{check}<<< please try again ")
-            check_field()
+            if check != "show" and check != "exit":
+                print(f"input error >>>{check}<<< please try again ")
+                check_field()
+        
+        if go_on >= (length**2):
+            end(True)
+        elif bombs_detected == bombs:
+            end(True)
+        elif not go_on >= (length**2) and not bombs_detected == bombs:
+            if did == False:
+                check_field()
 
+def end(win):
+    global check,did
+    check = ""
+    if win == False:
+        print("\nThe game is over!\n")
+        print("You have lost the game!")
+        did = True
+    if win == True:
+        print("\nThe game is over!\n")
+        print("You have won the game!\n")
+        did = True
+    rest = input("Do you want to play again? (y/n)\n<restart> ")
+    if rest == "y":
+        restart()
 
+def restart():
+    global check
+    check = ""
+    print("\n\n" + title + '\n')
+    print(format)
+    length = int(input('\nPlease select a grid size(recommended 3-7)\n<grid-size> '))
+        
+        
+    for i in range(length):
+        for k in range(length):
+            Layout[i][k] = field((i,k),t_f[randint(0,2)],0,False,False)
+            shown_Layout[i][k] = (i,k)
+                
+    for i in range(length):
+        for k in range(length):
+            bomb_check(i,k)
 
+    for i in range(length):
+        for k in range(length):
+            if Layout[i][k].bomb:
+                bombs += 1
+    while bombs == 0 or bombs == length**2:
+        bomb_define_again()
+            
+    game(False)
 
 if __name__ == "__main__":
     game(False)
